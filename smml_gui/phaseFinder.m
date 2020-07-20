c5717c9af80fc74075b8ea047b5354bfef4cd75d
OliverLab <wasserberger.kyle@gmail.com>
2020-07-20 10:18:45 -0500

    phaseFinder edits

--- smml_gui/phaseFinder.m
+++ smml_gui/phaseFinder.m
@@ -156,14 +156,27 @@ phaseMaster = renamevars(phaseMaster,["efiles","repphases","Frame_"],...
 aveFileNames = phaseMaster.participant(pStartRow);
 aveEventNames = phaseMaster.phase(1:numTrials:height(phaseMaster));
 
-avePhaseMaster = [ave_phase_1_master ; ave_phase_2_master ; ave_phase_3_master ; ave_phase_4_master];
+if numEvents == 5
+    avePhaseMaster = [ave_phase_1_master ; ave_phase_2_master ; ave_phase_3_master ; ave_phase_4_master];
 
+elseif numEvents == 4
+    avePhaseMaster = [ave_phase_1_master ; ave_phase_2_master ; ave_phase_3_master];
+    
+elseif numEvents == 3
+    avePhaseMaster = [ave_phase_1_master ; ave_phase_2_master];
+    
 
 % Give avePhaseMaster the same variable names and properties as phaseMaster
 avePhaseMaster.Properties.VariableNames = phaseMaster.Properties.VariableNames;
 
 avePhaseMaster.phase = aveEventNames;
-avePhaseMaster.participant = repmat(aveFileNames,4,1);
+
+if numEvents == 5
+    avePhaseMaster.participant = repmat(aveFileNames,4,1);
+elseif numEvents == 4
+    avePhaseMaster.participant = repmat(aveFileNames,3,1);
+elseif numEvents == 3
+    avePhaseMaster.participant = repmat(aveFileNames,2,1);
 
 %% Sort tables
 
